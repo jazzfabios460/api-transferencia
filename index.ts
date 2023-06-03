9a8f5fb5e2315d35b3a219e0cac467f54162e51c
jazzfabios460 <jazzfabios@gmail.com>
2023-06-02 23:08:44 -0300

    teste de deploy 8

--- index.ts
+++ index.ts
@@ -2,10 +2,7 @@ import Express from 'express'
 import router from './src/routes'
 const app =  Express()
 app.use(Express.json())
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.forEach(r=>{
     return app.use(r.endopoint, r.route)
 })
