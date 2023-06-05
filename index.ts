c25acc28b37b7df69bc2f7d957e9198bcafe2cfd
jazzfabios460 <jazzfabios@gmail.com>
2023-06-05 12:41:04 -0300

    fazendo o README

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import router from './src/routes'
 import cors from 'cors'
 import  swaggerUI from 'swagger-ui-express'
 import  swaggerDocument from './swagger.json'
+const css = { customCssUrl: '/public/css/swagger-ui.css',};
 const app =  Express()
 app.use(cors())
 app.use(Express.json())
@@ -10,5 +11,5 @@ router.forEach(r=>{
     return app.use(r.endopoint, r.route)
 })
 
-app.use("/swagger", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use("/swagger", swaggerUI.serve, swaggerUI.setup(swaggerDocument,css))
 app.listen(4000,()=>console.log('rodando na porta 4000'))
