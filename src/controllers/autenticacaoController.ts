12171bbea0f2d95e8b91a1686c0e5d19f07a0056
jazzfabios460 <jazzfabios@gmail.com>
2023-06-06 20:56:51 -0300

    fazendo o README teste versão prisma

--- src/controllers/autenticacaoController.ts
+++ src/controllers/autenticacaoController.ts
@@ -17,14 +17,14 @@ export const autenticar = async (req:Request, res:Response, next:NextFunction)=>
             id:true
            }
         })
-        // if (usuario) {
-        //     res.json({
-        //         token:Jwt.sign({user:usuario?.nome},secretKey,{expiresIn:"1d"}),
-        //         usuario
-        //     })
-        // }else{
-        //     res.status(401).json("Usuario ou senha inválidos!")
-        // }
+        if (usuario) {
+            res.json({
+                token:Jwt.sign({user:usuario?.nome},secretKey,{expiresIn:"1d"}),
+                usuario
+            })
+        }else{
+            res.status(401).json("Usuario ou senha inválidos!")
+        }
         res.json(usuario)
       } catch (error) {
         res.status(400).json(error)
