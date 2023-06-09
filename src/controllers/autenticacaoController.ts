b56b1485a95fd18486f3a650c6ac578b59f5e1c4
jazzfabios460 <jazzfabios@gmail.com>
2023-06-09 10:44:10 -0300

    alterando busca de usuario

--- src/controllers/autenticacaoController.ts
+++ src/controllers/autenticacaoController.ts
@@ -15,8 +15,12 @@ export const autenticar = async (req:Request, res:Response, next:NextFunction)=>
             email:true,
             nome:true,
             id:true,
-            conta:true
-           }
+            conta:{
+                select:{
+                    saldo:true
+                }
+            }
+           },
         })
         if (usuario) {
             res.json({
